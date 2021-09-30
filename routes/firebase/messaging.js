1193b0d9ff31247a0bb02f3c09cf61d7dab2be2b
nick <n.zaycev@rambler.ru>
2021-09-30 12:11:25 +0700

    Add fb log

--- routes/firebase/messaging.js
+++ routes/firebase/messaging.js
@@ -5,18 +5,68 @@ const messaging = require('./firebase_conf');
 const db = require('../../db/db');
 const jwt = require('jwt-simple');
 const config = require('../../config/config');
+var fs = require('fs');
+var util = require('util');
+
+router.post('/test', async (req, res) => {
+  console.log('firebase_test body', req.body)
+  console.log('firebase_test headers', req.headers)
+
+  var fb_log_file = fs.createWriteStream(__dirname  + '/firebase.log', { flags: 'a' })
+  // let client_ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.socket.remoteAddress || null
+  // console.log('client_ip', client_ip, req.ip)
+
+  let log_obj = 
+    new Date().toLocaleString() + ' '
+    + req.ip + ' '
+    + JSON.stringify(req.body)
+  fb_log_file.write(util.format(log_obj) + '\n');  
+  fb_log_file.write(log_obj + '\n');  
+
+  res.send().status(200);
+})
 
 router.post('/send', async (req, res) => {
-  console.log('firebase send body', req.body)
+  console.log('firebase_send body', req.body)
 
   // var user_token_data = jwt.decode(req.headers.auth, config.secret, false, 'HS256')
   // console.log('firebase send user_token', user_token_data)
 
-  const registrationTokens = ['cMRhMCitY5eUqeMMu1Lk-u:APA91bFg6mI6i50QZ4Typyz3k1Va45g4Ma5E1AHCyen-EAzz4Q2nDGazfW5DT2aPZSltjgbx4WL6wXauInsQGwLxb_JQAzmrnH4vbnz9-f2SV-f3iauMt7JN_bsW_eBuGbMecDBUATKn'];
+  let token_list = []
+
+  var filter_user = { 'email._V': 'test' };
+  let get_secure_fields = { ['fb_token']: 1 };
+  let get_user_fields = { 'email': 1 };
+  // console.log('firebase_send get_user_fields', get_user_fields)
+  // console.log('firebase_send get_secure_fields', get_secure_fields)
+
+  // Ищем юзера с email из jwt_token
+  db
+    .get(db.users_database, db.users_collection, filter_user, get_user_fields)
+    .then((get_users_results) => {
+      // console.log('firebase_send get_users_results', get_users_results)
+
+      var filter_secure = { 'user_id': get_users_results[0]._id };
+
+      // По нему ищем его объект в secure
+      db
+        .get(db.secure_database, db.secure_collection, filter_secure, get_secure_fields)
+        .then((get_secure_results) => {
+          // console.log('firebase_send get_secure_results', get_secure_results)
+
+          // // Достаём нужное поле по URL
+          token_list = Object.values(get_secure_results[0].fb_token)
+          // console.log('register_token token_list', token_list)
+        })
+        .catch((err) => {
+          next(err);
+        })
+    })
+
+  var registrationTokens = token_list;
   const imageUri = 'https://e7.pngegg.com/pngimages/826/677/png-clipart-butterfly-red-butterfly-brush-footed-butterfly-photography.png'
   const message = {
-    token: registrationTokens[0],
-
+    // token: registrationTokens[0],
     // notification: {
     //   body: 'This is an FCM notification that displays an image!',
     //   title: 'FCM Notification',
