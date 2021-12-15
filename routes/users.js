86b41bd866ad87dfdb82463d573189ed41eb4102
coprocoder <mog.ie@mail.ru>
2021-12-15 13:56:50 +0700

    Add profile delete mode

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jwt-simple");
+
 const db = require("../db/db");
-const {
-  val_key,
-  time_key,
-  wrap,
-  unwrap,
-} = require("../db/wrapper");
+const config = require("../config/config");
+const { val_key, time_key, wrap, unwrap } = require("../db/wrapper");
 
 /* === Select fields from ALL === */
 
@@ -76,53 +74,34 @@ router.post("/find", (req, res, next) => {
 
 /* === FULL ITEMS BLOCK === */
 
-// Create new user
-router.post("/create", (req, res, next) => {
-  console.log("CREATE ANY req.body", req.body);
-  res.redirect(307, "../../auth/signup"); // 307 не меняет метод и тело при редиректе
-});
-
 // Delete existed user
 router.post("/delete", (req, res, next) => {
   console.log("DELETE ANY req.body", req.body);
+  console.log("DELETE ANY req.headers", req.headers);
 
   var token_data = jwt.decode(req.headers.auth, config.secret, false, "HS256");
-  var filter = req.body;
-  var fields = {};
+  console.log("DELETE ANY token_data", token_data);
 
-  if (!!token_data && token_data.role == 0) {
-    db
-      // Ищем объект для удаления
-      .get(db.users_database, db.users_collection, filter, fields)
+  if (!!token_data && (token_data.role == 0 || unwrap(token_data.email) == req.body.email)) {
+    var filter = { ["email." + val_key]: req.body.email };
+    var fields_user = { _id: 1 };
+
+    // Remove profile
+    db.get(db.users_database, db.users_collection, filter, fields_user)
       .then((get_results) => {
-        // console.log('DELETE ANY USER results', get_results)
-        db
-          // Удаляем объекта из обычной БД
-          .delete(db.users_database, db.users_collection, filter)
+        db.delete(db.users_database, db.users_collection, filter)
           .then((del_user_results) => {
-            // Ищем в секретной БД связанный с юзером объект и удаляем его
-            var secure_filter = { "user_id.value": get_results[0]._id };
-            db.delete(db.secure_database, db.secure_collection, secure_filter)
-              .then((del_secure_results) => {
-                if (!!del_secure_results) {
-                  res.send(del_secure_results);
-                } else {
-                  const err = new Error("Данные не найдены!");
-                  err.status = 400;
-                  next(err);
-                }
-              })
-              .catch((err) => {
-                next(err);
-              });
+            db.delete(db.secure_database, db.secure_collection, { user_id: get_results[0]._id }).catch((err) =>
+              next(err)
+            );
           })
-          .catch((err) => {
-            next(err);
-          });
+          .catch((err) => next(err));
       })
-      .catch((err) => {
-        next(err);
-      });
+      .catch((err) => next(err));
+
+    db.delete(db.users_database, db.diaries_collection, filter).catch((err) => next(err));
+    db.delete(db.users_database, db.medicaments_collection, filter).catch((err) => next(err));
+    db.delete(db.users_database, db.schedules_collection, filter).catch((err) => next(err));
   } else {
     const err = new Error("Нет прав на удаление!");
     err.status = 403;
